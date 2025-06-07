7b46e11813e335343dd235516960dd31a934ca73
victor <victor.vrigstad@gmail.com>
2025-06-07 19:16:41 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void dog()
+{
+    while (1 != 2)
+    {
+        std::cout << "lucas dog" << std::endl;
+    }
+}
+
+int main()
+{
+    dog();
+    //std::cout << "hello world" << std::endl;
+    return 0;
+}
